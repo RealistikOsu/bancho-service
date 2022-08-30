44befe59e8c8c1e8c3843b84cb3b12db5ace11a0
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-08-30 23:27:39 +0100

    start privileges

---
+++ app/constants/privileges.py
@@ -0,0 +1,15 @@
+from enum import IntFlag
+
+class Privileges(IntFlag):
+    LOGIN = 1 << 0
+    PROFILE_PUBLIC = 1 << 1
+    SEND_PUBLIC_MESSAGE = 1 << 2
+    SEND_PRIVATE_MESSAGE = 1 << 3
+    SCORES_PUBLIC = 1 << 4
+    SET_USERPAGE = 1 << 5
+    SET_AVATAR = 1 << 6
+    CREATE_MATCH = 1 << 7
+    JOIN_MATCH = 1 << 8
+    RANKING_REQUEST = 1 << 9
+    SET_BANNER = 1 << 10
+    CREATE_USER_COMMENTS = 1 << 11
