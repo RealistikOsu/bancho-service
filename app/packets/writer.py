b86359d9499aa2d261986a6bcb7121571b189f51
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-08-31 20:18:48 +0100

    string writing

--- app/packets/writer.py
+++ app/packets/writer.py
@@ -57,3 +57,23 @@ class WritableBuffer:
 
     def write_f32(self, val: float) -> None:
         self._buffer += struct.pack("<f", val)
+
+    def write_raw(self, val: bytes) -> None:
+        self._buffer += val
+
+    def write_uleb128(self, val: int) -> None:
+        while val > 0:
+            self.write_u8(val & 0b1111111)
+            val >>= 7
+            if val > 0:
+                self._buffer[-1] |= 1 << 7
+
+    def write_osu_str(self, val: str) -> None:
+        if not val:
+            self.write_u8(0x0)
+            return
+
+        string_bytes = val.encode("utf-8")
+        self.write_u8(0xB)
+        self.write_uleb128(len(string_bytes))
+        self.write_raw(string_bytes)
